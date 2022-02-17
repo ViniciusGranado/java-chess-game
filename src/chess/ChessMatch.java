a476d46dfcf6c9cee91bf098ac3e221a0e271ccd
Vinicius Granado <vinicius.r.granado@gmail.com>
2022-02-17 20:58:55 -0300

    Added king possible moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -28,6 +28,7 @@ public class ChessMatch {
 
   public boolean[][] possibleMoves(ChessPosition sourcePosition) {
     Position position = sourcePosition.toPosition();
+    validateSourcePosition(position);
     return board.piece(position).possibleMoves();
   }
 
